82e7c1a0befc39fb55dcd389a4a79cebdd90123a
Country.Girls <country.girls@109-b-0m3gfwm-m.manorisd.net>
2017-01-21 14:33:56 -0600

    initial commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Country.Girls on 1/21/17.
+//  Copyright © 2017 Country.Girls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var banner: UIImageView!
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var welcomeBtn: UIButton!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func welcomePressed(_ sender: Any) {
+        
+        background.isHidden=false
+        banner.isHidden=false
+        welcomeBtn.isHidden=true
+        
+    }
+
+}
+
